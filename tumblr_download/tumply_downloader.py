82c11793f075d83bd3a1918cc548acc81096c3e0
longqi <longqi90@gmail.com>
2017-02-02 17:14:22 +0800

    rewrite as class

--- tumblr_download/tumply_downloader.py
+++ tumblr_download/tumply_downloader.py
@@ -6,6 +6,7 @@ Description:
 
 
 """
+import argparse
 from itertools import count
 from multiprocessing.pool import Pool
 from os import mkdir
@@ -14,48 +15,71 @@ from pprint import pprint
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
 
-from .tumblr_api import Tumblpy2
+from tumblr_api import Tumblpy2
 
 
-def retrieve_and_save(url, name, path=None):
-	filename = join(path, name) if path else name
-	if exists(filename):
-		return
-	print('Downloading: ', url)
-	try:
-		urlretrieve(url, filename)
-	except:
-		print('Downloading failed', url)
+class TumblrDownload():
+	def __init__(self):
+		self.downloader_pool = Pool(processes=5)  # use 5 processes to download the data
+		self.client = Tumblpy2(
+				'zLgPh6LeV7DyczfPALkTEfr8rOgzcYAY8TzAlabVIYrgpATPON',
+				'mGP5mVle2ZUNKHzK4ayjAGpfUCkLTmQm91ic9YtWTTcDkdFLPE',
+				'hRwAn1CoZJ5Q96T8o51aQL2YcKnh1k66RlnCRLQtqjtWf0WZ4W',
+				'oqlple5FP9MVRTxbUQHjrEVSs4DDLFP7h4zBE5D4g952qeqRo3'
+		)
+		self.user_info = self.client.get('user/info')
+		self.blog_name = self.user_info['user']['name']
+		self.blog_url = self.user_info['user']['blogs'][0]['url'][8:-1]
 
+	@staticmethod
+	def retrieve_and_save(url, name, path=None):
+		filename = join(path, name) if path else name
+		if exists(filename):
+			return
+		print('Downloading: ', url)
+		try:
+			urlretrieve(url, filename)
+		except:
+			print('Downloading failed', url)
 
-def get_all_posts(client, blog_url, post_type=None):
-	ress = []
+	def get_all_posts(self, client, blog_url, post_type=None):
+		ress = []
 
-	r = client.posts(blog_url, post_type=post_type, limit=1, offset=0)
-	print('You have', r['total_posts'], 'video posts')
+		r = client.posts(blog_url, post_type=post_type, limit=1, offset=0)
+		print('You have', r['total_posts'], 'video posts')
 
-	ress.append(r)
-
-	for i in count():
-		r = client.posts(blog_url, post_type=post_type, limit=50, offset=i * 50)
 		ress.append(r)
-		if i * 50 > r['total_posts']:
-			break
-	return ress
+
+		for i in count():
+			r = client.posts(blog_url, post_type=post_type, limit=50, offset=i * 50)
+			ress.append(r)
+			if i * 50 > r['total_posts']:
+				break
+		return ress
+
+	def to_download(self):
+		download_list = []
+		try:
+			mkdir(self.blog_url)
+		except FileExistsError:
+			print('Directory exist', self.blog_url)
+		else:
+			print('Create dir for blog', self.blog_url)
+
+		for res in self.get_all_posts(self.client, self.blog_url, 'video'):
+			for post in res['posts']:
+				if 'video_url' in post.keys():
+					video_url = post['video_url']
+				else:
+					continue
+
+				filename = basename(urlparse(video_url).path)
+				download_list.append((video_url, filename, self.blog_url))
+
+		pprint(download_list)
+		self.downloader_pool.starmap(self.retrieve_and_save, download_list)
 
 
-downloader_pool = Pool(processes=5)  # use 5 processes to download the data
-
-client = Tumblpy2(
-		'zLgPh6LeV7DyczfPALkTEfr8rOgzcYAY8TzAlabVIYrgpATPON',
-		'mGP5mVle2ZUNKHzK4ayjAGpfUCkLTmQm91ic9YtWTTcDkdFLPE',
-		'hRwAn1CoZJ5Q96T8o51aQL2YcKnh1k66RlnCRLQtqjtWf0WZ4W',
-		'oqlple5FP9MVRTxbUQHjrEVSs4DDLFP7h4zBE5D4g952qeqRo3'
-)
-
-# Make the request
-user_info = client.get('user/info')
-print(user_info)
 # blog_url = blog_url['user']['blogs'][0]['url']
 # print(blog_url)
 #
@@ -63,27 +87,12 @@ print(user_info)
 #
 # followers= client.get('user/following')
 # print(followers)
-blog_name = user_info['user']['name']
-blog_url = user_info['user']['blogs'][0]['url'][8:-1]
-download_list = []
 
-try:
-	mkdir(blog_url)
-except FileExistsError:
-	print('Directory exist', blog_url)
-else:
-	print('Create dir for blog', blog_url)
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--path', '-p', action='store',
+											help='The root path that store all the files. eg. videos, audios, photos')
+	args = parser.parse_args()
 
-for res in get_all_posts(client, blog_url, 'video'):
-	for post in res['posts']:
-		if 'video_url' in post.keys():
-			video_url = post['video_url']
-		else:
-			continue
-
-		filename = basename(urlparse(video_url).path)
-
-		download_list.append((video_url, filename, blog_url))
-
-pprint(download_list)
-output = downloader_pool.starmap(retrieve_and_save, download_list)
+	loader = TumblrDownload()
+	loader.to_download()
