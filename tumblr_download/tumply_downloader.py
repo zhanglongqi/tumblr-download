0e6868f8541fc9a462c3668b1455bdcaced56300
longqi <longqi90@gmail.com>
2017-02-03 17:00:56 +0800

    support video post

--- tumblr_download/tumply_downloader.py
+++ tumblr_download/tumply_downloader.py
@@ -19,7 +19,7 @@ from tumblr_api import Tumblpy2
 
 
 class TumblrDownload():
-	def __init__(self):
+	def __init__(self, blog_url, path=None, post_type=None):
 		self.downloader_pool = Pool(processes=5)  # use 5 processes to download the data
 		self.client = Tumblpy2(
 				'zLgPh6LeV7DyczfPALkTEfr8rOgzcYAY8TzAlabVIYrgpATPON',
@@ -27,9 +27,13 @@ class TumblrDownload():
 				'hRwAn1CoZJ5Q96T8o51aQL2YcKnh1k66RlnCRLQtqjtWf0WZ4W',
 				'oqlple5FP9MVRTxbUQHjrEVSs4DDLFP7h4zBE5D4g952qeqRo3'
 		)
-		self.user_info = self.client.get('user/info')
-		self.blog_name = self.user_info['user']['name']
-		self.blog_url = self.user_info['user']['blogs'][0]['url'][8:-1]
+		# self.user_info = self.client.get('user/info')
+		# self.blog_name = self.user_info['user']['name']
+		# self.blog_url = self.user_info['user']['blogs'][0]['url'][8:-1]
+
+		self.blog_url = blog_url
+		self.path = path
+		self.post_type = post_type
 
 	@staticmethod
 	def retrieve_and_save(url, name, path=None):
@@ -46,7 +50,7 @@ class TumblrDownload():
 		ress = []
 
 		r = client.posts(blog_url, post_type=post_type, limit=1, offset=0)
-		print('You have', r['total_posts'], 'video posts')
+		print('You have', r['total_posts'], self.post_type, ' posts')
 
 		ress.append(r)
 
@@ -66,15 +70,16 @@ class TumblrDownload():
 		else:
 			print('Create dir for blog', self.blog_url)
 
-		for res in self.get_all_posts(self.client, self.blog_url, 'video'):
+		for res in self.get_all_posts(self.client, self.blog_url, self.post_type):
 			for post in res['posts']:
-				if 'video_url' in post.keys():
-					video_url = post['video_url']
+				if post['type'] == 'video':
+					url = post['video_url']
+				# todo support all the post types
 				else:
 					continue
 
-				filename = basename(urlparse(video_url).path)
-				download_list.append((video_url, filename, self.blog_url))
+				filename = basename(urlparse(url).path)
+				download_list.append((url, filename, self.blog_url))
 
 		pprint(download_list)
 		self.downloader_pool.starmap(self.retrieve_and_save, download_list)
@@ -90,10 +95,16 @@ class TumblrDownload():
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('blog', required=True)
-	parser.add_argument('--path', '-p', action='store',
-											help='The root path that store all the files. eg. videos, audios, photos')
-	args = parser.parse_args()
 
-	loader = TumblrDownload()
+	parser.add_argument('blog')
+	parser.add_argument('--type', '-t', action='store',
+											help='The posts type you want to download:'
+													 'Text,Photo,Quote,Link,Chat, Audio, Video,Answer')
+	parser.add_argument('--path', '-p', action='store',
+											help='The directory that store all the files you download.')
+	args = parser.parse_args()
+	if str.lower(str(args.type)) not in ['text', 'photo', 'quote', 'link', 'chat', 'audio', 'video', 'answer']:
+		print('You can only select type in Text, Photo, Quote, Link, Chat, Audio, Video, Answer')
+		exit(1)
+	loader = TumblrDownload(args.blog, path=args.path, post_type=args.type)
 	loader.to_download()
