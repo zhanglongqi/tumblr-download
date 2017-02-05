d5c7c107b1de1dcc497d337eedf0df80af94a8de
longqi <longqi90@gmail.com>
2017-02-05 14:54:41 +0800

    version 2.0.1

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ from setuptools import setup
 setup(
 		name='tumblr-download',
 		packages=['tumblr-download'],
-		version='1.6.1',
+		version='2.0.1',
 		install_requires=['python-tumblpy>=1.0.5'],
 		description='Download posts from tumblr blog',
 		author='Zhang LongQi',
 		author_email='longqi90@gmail.com',
 		url='https://github.com/zhanglongqi/tumblr-download',
-		download_url='https://github.com/zhanglongqi/tumblr-download/tarball/v1.6.1',
+		download_url='https://github.com/zhanglongqi/tumblr-download/tarball/v2.0.1',
 		keywords=['tumblr', 'download', 'backup', 'copy'],
 		classifiers=[],
 )
